7605c97cea7e100f84b6aa29ce2e15f80ff7f836
DobariyaDixita <dixitadobariya100@gmail.com>
2024-08-12 11:13:51 -0400

    Add rxJava in viewmodel class for reference

--- app/src/main/java/com/example/carfax/presentation/car/CarViewModel.kt
+++ app/src/main/java/com/example/carfax/presentation/car/CarViewModel.kt
@@ -5,6 +5,8 @@ import androidx.lifecycle.viewModelScope
 import com.example.carfax.domain.usecase.GetAllCarsUsecase
 import com.example.carfax.domain.util.Resource
 import dagger.hilt.android.lifecycle.HiltViewModel
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -42,10 +44,12 @@ class CarViewModel @Inject constructor(
                                     isLoading = false
                                 )
                             }
+
                             is Resource.Error -> {
                                 _state.value =
                                     state.value.copy(error = result.message, isLoading = false)
                             }
+
                             is Resource.Loading -> {
                                 _state.value = state.value.copy(isLoading = result.isLoading)
                             }
@@ -58,3 +62,47 @@ class CarViewModel @Inject constructor(
 
 
 }
+
+
+/* rxjava code for reference
+private val disposables = CompositeDisposable()
+
+
+
+fun loadCategory() {
+    _state.value = state.value.copy(isLoading = true)
+
+    disposables.add(
+        getAllCarsUsecase()
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .doOnSubscribe { _state.value = state.value.copy(isLoading = true) }
+            .doFinally { _state.value = state.value.copy(isLoading = false) }
+            .subscribe(
+                { result ->
+                    when (result) {
+                        is Resource.Success -> {
+                            _state.value = CarInfoState(
+                                carList = result.data ?: emptyList(),
+                                isLoading = false
+                            )
+                        }
+                        is Resource.Error -> {
+                            _state.value = state.value.copy(error = result.message, isLoading = false)
+                        }
+                        is Resource.Loading -> {
+                            _state.value = state.value.copy(isLoading = result.isLoading)
+                        }
+                    }
+                },
+                { error ->
+                    _state.value = state.value.copy(error = "Exception: ${error.message}", isLoading = false)
+                }
+            )
+    )
+}
+
+override fun onCleared() {
+    super.onCleared()
+    disposables.dispose()
+}*/
